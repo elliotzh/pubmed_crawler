e6961109464b5877e13d9d6cc64dcfdc91c51326
xingzh <xingzh@microsoft.com>
2021-02-14 18:03:31 +0800

    refactor: add multiprocessor & use class to control different sources

--- crawler.py
+++ crawler.py
@@ -1,219 +1,189 @@
-import urllib3
-import datetime
-from os import path
-import os
-import threading
-import json
-from multiprocessing.pool import ThreadPool
 from bs4 import BeautifulSoup
+from multiprocessor import MultiProcessor
+from os import path
+import json
+import os
 
+class NatureCrawler:
+    def __init__(self, processor: MultiProcessor, journal_name: str, data_dir: str):
+        self._processor = processor
+        self._journal_name = journal_name
+        self._data_dir = data_dir
 
-class Scraper:
-    def __init__(self, processes=20, chunk_size=100):
-        self._thread_pool = ThreadPool(processes=processes)
-        self._chunk_size = chunk_size
+    @property
+    def journal_name(self) -> str:
+        return self._journal_name
 
-        # temp
-        self._local_dir = ""
-        self._url_func = lambda x: x
-        self._target_suffix = ""
+    @property
+    def data_dir(self) -> str:
+        return self._data_dir
 
-        self._process_func = lambda x: x
+    @property
+    def processor(self) -> MultiProcessor:
+        return self._processor
 
-    def scrape_all(self, local_dir, target_names, url_func, early_stop_func=None, target_suffix="html"):
-        self._url_func = url_func
-        self._local_dir = local_dir
-        self._target_suffix = target_suffix
-
-        if not path.isdir(self._local_dir):
-            os.makedirs(self._local_dir)
-
-        max_i = -1
-        if early_stop_func is not None:
-            self.scrape_one(target_names[0])
-            with open(self.get_target_path(target_names[0]), "r", encoding="utf-8") as first_page_file:
-                max_i = early_stop_func(first_page_file.read())
-                first_page_file.close()
-            assert max_i <= 1000
-
-        for i, _ in enumerate(self._thread_pool.imap(self.scrape_one, target_names[:max_i], chunksize=self._chunk_size)):
-            if divmod(i, 1000)[1] == 0:
-                print("#{}: {}".format(i, datetime.datetime.now()))
-
-    def get_target_path(self, target_name):
-        return path.join(self._local_dir, "{}.{}".format(target_name, self._target_suffix))
-
-    def scrape_one(self, target_name):
-        if path.isfile(self.get_target_path(target_name)):
-            return target_name
-
-        url_pool = urllib3.PoolManager()
-        target_url = self._url_func(target_name)
-        if target_url is None:
-            print("Can't find source link for pubmed doc id {}".format(target_name))
-            return target_name
-
-        try:
-            r = url_pool.request("GET", target_url)
-        except urllib3.exceptions.MaxRetryError:
-            print("can't access pubmed doc {}".format(target_name))
-            return target_name
-        with open(self.get_target_path(target_name), "wb") as html_file:
-            html_file.write(r.data)
-            html_file.close()
-        return target_name
-
-    def process_one(self, target_name):
-        input_path = self._url_func(target_name)
-        output_path = path.join(self._local_dir, "{}.{}".format(target_name, self._target_suffix))
-        if not path.isfile(input_path):
-            return
-        with open(input_path, "r", encoding="utf-8") as infile:
-            try:
-                output = self._process_func(infile.read())
-            except:
-                infile.close()
-                print("Met error in parsing pubmed doc {}.".format(target_name))
-                return
-            infile.close()
-
-            if output is not None:
-                with open(output_path, "w", encoding="utf-8") as outfile:
-                    json.dump(output, outfile, indent=2)
-                    outfile.close()
-
-    def process_all(self, input_func, output_dir, target_names, process_func, target_suffix="json"):
-        self._process_func = process_func
-        self._url_func = input_func
-        self._local_dir = output_dir
-        self._target_suffix = target_suffix
-
-        if not path.isdir(self._local_dir):
-            os.makedirs(self._local_dir)
-
-        for i, _ in enumerate(
-                self._thread_pool.imap(self.process_one, target_names, chunksize=self._chunk_size)):
-            if divmod(i, 1000)[1] == 0:
-                print("#{}: {}".format(i, datetime.datetime.now()))
-
-
-def get_max_page(content):
-    soup = BeautifulSoup(content, features="html.parser")
-    amount = soup.find("div", {"class": "results-amount"})
-    item_count = int(amount.span.text.replace(',', ''))
-    return divmod(item_count, 10)[0] + 1
-
-
-def get_source_link(i, base_dir, journal_name):
-    with open("{}\\{}.html".format(base_dir, i), "r", encoding="utf-8") as pubmed_file:
-        content = pubmed_file.read()
-        if content.find("full-text-links-list") is -1:
-            return None
-        pubmed_file.close()
+    @classmethod
+    def get_max_page(cls, content):
         soup = BeautifulSoup(content, features="html.parser")
-        link_div = soup.find("div", {"class": "full-text-links-list"})
-        if link_div is None:
+        amount = soup.find("div", {"class": "results-amount"})
+        item_count = int(amount.span.text.replace(',', ''))
+        return divmod(item_count, 10)[0] + 1
+
+    @classmethod
+    def get_source_link(cls, i, base_dir):
+        with open("{}\\{}.html".format(base_dir, i), "r", encoding="utf-8") as pubmed_file:
+            content = pubmed_file.read()
+            if content.find("full-text-links-list") is -1:
+                return None
+            pubmed_file.close()
+            soup = BeautifulSoup(content, features="html.parser")
+            link_div = soup.find("div", {"class": "full-text-links-list"})
+            if link_div is None:
+                return None
+        return cls.parse_origin_link(link_div.a["href"])
+
+    @classmethod
+    def parse_origin_link(cls, origin_url):
+        nature_id = origin_url.split("nature")[-1]
+        return "https://www.nature.com/articles/nature{}".format(nature_id)
+
+    def get_info(self, content):
+        info_dict = {}
+        if content.find("No abstract available") is not -1:
             return None
-        origin_url = link_div.a["href"]
-        if journal_name == "Nature":
-            nature_id = origin_url.split("nature")[-1]
-            return "https://www.nature.com/articles/nature{}".format(nature_id)
-        elif journal_name in ["Nature Methods", "Nature Communications", "Nature Biotechnology"]:
-            nature_id = origin_url.split("/")[-1]
-            return "https://www.nature.com/articles/{}".format(nature_id)
-        return None
+        if content.find("Sorry, the page you requested is unavailable. "
+                        "The link you requested might be broken, or no longer exist.") is not -1:
+            return None
+        soup = BeautifulSoup(content, features="html.parser")
 
+        info_dict["Type"] = self.get_doc_type(soup)
 
-def get_nature_info(content, journal_name):
-    info_dict = {}
-    if content.find("No abstract available") is not -1:
-        return None
-    if content.find("Sorry, the page you requested is unavailable. The link you requested might be broken, or no longer exist.") is not -1:
-        return None
-    soup = BeautifulSoup(content, features="html.parser")
+        title = soup.find("h1", {"class": "c-article-title"})
+        info_dict["Title"] = title.text
 
-    if journal_name == "Nature":
+        abstract = soup.find("div", {"id": "Abs1-content"})
+        if abstract is None:
+            return None
+        info_dict["Abstract"] = abstract.text
+        return info_dict
+
+    @classmethod
+    def get_doc_type(cls, soup):
         breadcrumb = soup.find("li", {"id": "breadcrumb1"})
-    else:
+        return breadcrumb.span.text
+
+    def scrape_index(self, year):
+        self.processor.scrape_all(
+            "{}{}\\{}".format(self.data_dir, self.journal_name, year),
+            range(1, 1000),
+            lambda x: "https://pubmed.ncbi.nlm.nih.gov/?term=((%22{}%2F01%2F01%22%5BDate%20-%20Publication" \
+                      "%5D%20%3A%20%22{}%2F12%2F31%22%5BDate%20-%20Publication%5D))%20AND%20(%22" \
+                      "{}%22%5BJournal%5D)&sort=&page={}".format(year, year, self.journal_name.replace(" ", "+"), x),
+            early_stop_func=self.get_max_page
+        )
+        print("Index scraped.")
+        return True
+
+    def extract_detail_page_list(self, year):
+        base_path = "{}{}\\{}".format(self.data_dir, self.journal_name, year)
+        if not path.isdir(base_path):
+            return None
+        doc_ids_path = base_path + ".pubmed.json"
+        if path.isfile(doc_ids_path):
+            with open(doc_ids_path, "r", encoding="utf-8") as doc_ids_file:
+                yearly_doc_ids = json.load(doc_ids_file)
+                doc_ids_file.close()
+        else:
+            yearly_doc_ids = []
+            for base, dirs, files in os.walk(base_path):
+                for file in files:
+                    with open(path.join(base, file), "r", encoding="utf-8") as index_file:
+                        soup = BeautifulSoup(index_file.read(), features="html.parser")
+                        index_file.close()
+                        for item in soup.find_all("a", {"class": "docsum-title"}):
+                            yearly_doc_ids.append(item["href"].strip("/"))
+            with open(doc_ids_path, "w", encoding="utf-8") as doc_ids_file:
+                json.dump(yearly_doc_ids, doc_ids_file)
+                doc_ids_file.close()
+        print("Detail page list parsed.")
+        return yearly_doc_ids
+
+    @property
+    def pubmed_dir(self) -> str:
+        return "{}{}\\all_pubmed".format(self.data_dir, self.journal_name)
+
+    @property
+    def source_dir(self) -> str:
+        return "{}{}\\all_source".format(self.data_dir, self.journal_name)
+
+    def scrape_pubmed_detail_pages(self, yearly_doc_ids):
+        self.processor.scrape_all(
+            self.pubmed_dir,
+            yearly_doc_ids,
+            lambda x: "https://pubmed.ncbi.nlm.nih.gov/{}/".format(x)
+        )
+        print("Detail pages scraped.")
+
+    def scrape_source_detail_pages(self, yearly_doc_ids):
+        self.processor.scrape_all(
+            self.source_dir,
+            yearly_doc_ids,
+            lambda x: self.get_source_link(x, self.pubmed_dir)
+        )
+        print("Source pages scraped.\n")
+
+    def extract_info(self, yearly_doc_ids):
+        result_dir = "E:\\temp\\secret\\{}\\all_results".format(self.journal_name)
+
+        self.processor.process_all(
+            input_func=lambda x: "{}\\{}.html".format(self.source_dir, x),
+            output_dir=result_dir,
+            target_names=yearly_doc_ids,
+            process_func=lambda x: self.get_info(x)
+        )
+
+
+class NatureSubCrawler(NatureCrawler):
+    def __init__(self, processor: MultiProcessor, journal_name: str, data_dir: str):
+        super(NatureSubCrawler, self).__init__(processor, journal_name, data_dir)
+
+    @classmethod
+    def parse_origin_link(cls, origin_url):
+        nature_id = origin_url.split("/")[-1]
+        return "https://www.nature.com/articles/{}".format(nature_id)
+
+    @classmethod
+    def get_doc_type(cls, soup):
         breadcrumb = soup.find("li", {"id": "breadcrumb2"})
-    info_dict["Type"] = breadcrumb.span.text
-
-    title = soup.find("h1", {"class": "c-article-title"})
-    info_dict["Title"] = title.text
-
-    abstract = soup.find("div", {"id": "Abs1-content"})
-    if abstract is None:
-        return None
-    info_dict["Abstract"] = abstract.text
-    return info_dict
+        return breadcrumb.span.text
 
 
 def __main__():
-    scraper = Scraper()
+    processor = MultiProcessor()
     data_dir = "E:\\temp\\secret\\"
 
     journal_names = ["Nature", "Nature Methods", "Nature Communications", "Nature Biotechnology"]
     for journal_name in journal_names:
+        if journal_name == "Nature":
+            crawler = NatureCrawler(processor, journal_name, data_dir)
+        elif journal_name.startswith("Nature"):
+            crawler = NatureSubCrawler(processor, journal_name, data_dir)
+        else:
+            continue
+
         # all docs
         for year in range(2010, 2021):
             print("Year:{} Journal: {}".format(year, journal_name))
-            scraper.scrape_all(
-                "{}{}\\{}".format(data_dir, journal_name, year),
-                range(1, 1000),
-                lambda x: "https://pubmed.ncbi.nlm.nih.gov/?term=((%22{}%2F01%2F01%22%5BDate%20-%20Publication" \
-                "%5D%20%3A%20%22{}%2F12%2F31%22%5BDate%20-%20Publication%5D))%20AND%20(%22" \
-                "{}%22%5BJournal%5D)&sort=&page={}".format(year, year, journal_name.replace(" ", "+"), x),
-                early_stop_func=get_max_page
-            )
-            print("Index scraped.")
+            crawler.scrape_index(year)
 
-            base_path = "{}{}\\{}".format(data_dir, journal_name, year)
-            if not path.isdir(base_path):
+            yearly_doc_ids = crawler.extract_detail_page_list(year)
+            if yearly_doc_ids is None:
                 continue
-            doc_ids_path = base_path + ".pubmed.json"
-            if path.isfile(doc_ids_path):
-                with open(doc_ids_path, "r", encoding="utf-8") as doc_ids_file:
-                    yearly_doc_ids = json.load(doc_ids_file)
-                    doc_ids_file.close()
-            else:
-                yearly_doc_ids = []
-                for base, dirs, files in os.walk(base_path):
-                    for file in files:
-                        with open(path.join(base, file), "r", encoding="utf-8") as index_file:
-                            soup = BeautifulSoup(index_file.read(), features="html.parser")
-                            index_file.close()
-                            for item in soup.find_all("a", {"class": "docsum-title"}):
-                                yearly_doc_ids.append(item["href"].strip("/"))
-                with open(doc_ids_path, "w", encoding="utf-8") as doc_ids_file:
-                    json.dump(yearly_doc_ids, doc_ids_file)
-                    doc_ids_file.close()
-            print("Detail page list parsed.")
 
-            pubmed_dir = "{}{}\\all_pubmed".format(data_dir, journal_name)
-            scraper.scrape_all(
-                pubmed_dir,
-                yearly_doc_ids,
-                lambda x: "https://pubmed.ncbi.nlm.nih.gov/{}/".format(x)
-            )
-            print("Detail pages scraped.")
+            crawler.scrape_pubmed_detail_pages(yearly_doc_ids)
+            crawler.scrape_source_detail_pages(yearly_doc_ids)
 
-            source_dir = "{}{}\\all_source".format(data_dir, journal_name)
-            scraper.scrape_all(
-                source_dir,
-                yearly_doc_ids,
-                lambda x: get_source_link(x, pubmed_dir, journal_name)
-            )
-            print("Source pages scraped.\n")
-
-            result_dir = "E:\\temp\\secret\\{}\\all_results".format(journal_name)
-
-            if journal_name == "Nature":
-                scraper.process_all(
-                    input_func=lambda x: "{}\\{}.html".format(source_dir, x),
-                    output_dir=result_dir,
-                    target_names=yearly_doc_ids,
-                    process_func=lambda x: get_nature_info(x, journal_name)
-                )
+            crawler.extract_info(yearly_doc_ids)
 
 
 if __name__ == "__main__":
---
+++ multiprocessor.py
@@ -0,0 +1,99 @@
+import urllib3
+import datetime
+from os import path
+import os
+import threading
+import json
+from multiprocessing.pool import ThreadPool
+from bs4 import BeautifulSoup
+
+
+class MultiProcessor:
+    def __init__(self, processes=20, chunk_size=100):
+        self._thread_pool = ThreadPool(processes=processes)
+        self._chunk_size = chunk_size
+
+        # temp
+        self._local_dir = ""
+        self._url_func = lambda x: x
+        self._target_suffix = ""
+
+        self._process_func = lambda x: x
+
+    def scrape_all(self, local_dir, target_names, url_func, early_stop_func=None, target_suffix="html"):
+        self._url_func = url_func
+        self._local_dir = local_dir
+        self._target_suffix = target_suffix
+
+        if not path.isdir(self._local_dir):
+            os.makedirs(self._local_dir)
+
+        max_i = -1
+        if early_stop_func is not None:
+            self.scrape_one(target_names[0])
+            with open(self.get_target_path(target_names[0]), "r", encoding="utf-8") as first_page_file:
+                max_i = early_stop_func(first_page_file.read())
+                first_page_file.close()
+            assert max_i <= 1000
+
+        for i, _ in enumerate(self._thread_pool.imap(self.scrape_one, target_names[:max_i], chunksize=self._chunk_size)):
+            if divmod(i, 1000)[1] == 0:
+                print("#{}: {}".format(i, datetime.datetime.now()))
+
+    def get_target_path(self, target_name):
+        return path.join(self._local_dir, "{}.{}".format(target_name, self._target_suffix))
+
+    def scrape_one(self, target_name):
+        if path.isfile(self.get_target_path(target_name)):
+            return target_name
+
+        url_pool = urllib3.PoolManager()
+        target_url = self._url_func(target_name)
+        if target_url is None:
+            print("Can't find source link for pubmed doc id {}".format(target_name))
+            return target_name
+
+        try:
+            r = url_pool.request("GET", target_url)
+        except urllib3.exceptions.MaxRetryError:
+            print("can't access pubmed doc {}".format(target_name))
+            return target_name
+        with open(self.get_target_path(target_name), "wb") as html_file:
+            html_file.write(r.data)
+            html_file.close()
+        return target_name
+
+    def process_one(self, target_name):
+        input_path = self._url_func(target_name)
+        output_path = path.join(self._local_dir, "{}.{}".format(target_name, self._target_suffix))
+        if not path.isfile(input_path):
+            return
+        with open(input_path, "r", encoding="utf-8") as infile:
+            try:
+                output = self._process_func(infile.read())
+            except:
+                infile.close()
+                print("Met error in parsing pubmed doc {}.".format(target_name))
+                return
+            infile.close()
+
+            if output is not None:
+                with open(output_path, "w", encoding="utf-8") as outfile:
+                    json.dump(output, outfile, indent=2)
+                    outfile.close()
+
+    def process_all(self, input_func, output_dir, target_names, process_func, target_suffix="json"):
+        self._process_func = process_func
+        self._url_func = input_func
+        self._local_dir = output_dir
+        self._target_suffix = target_suffix
+
+        if not path.isdir(self._local_dir):
+            os.makedirs(self._local_dir)
+
+        for i, _ in enumerate(
+                self._thread_pool.imap(self.process_one, target_names, chunksize=self._chunk_size)):
+            if divmod(i, 1000)[1] == 0:
+                print("#{}: {}".format(i, datetime.datetime.now()))
+
+
